d2e631ba2a006c324c6722bbf7b4041a14d2fb9a
codeaway <codealltheway@gmail.com>
2012-02-01 15:13:37 -0500

    first commit

---
+++ src/main/java/com/codeaway/ejb/mdb/App.java
@@ -0,0 +1,13 @@
+package com.codeaway.ejb.mdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
